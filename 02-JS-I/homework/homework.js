976c0de91bd1c56306d5bad26b2e45e8f0b04c12
Bienvenido <Bienvenido@RYZEN-5>
2022-01-22 21:19:40 -0300

    Problemas resueltos JS1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -205,7 +205,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return "Hola "clear+nombre+"!"
+  return "Hola "+nombre+"!"
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
